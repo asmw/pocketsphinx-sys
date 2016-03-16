44edfe9678b564c6b7634b025f439b9d1d359be1
Mikhail Trishchenkov <kriomant@gmail.com>
2016-03-16 23:22:58 +0600

    Fix anytype_t declaration

--- src/glist.rs
+++ src/glist.rs
@@ -1,8 +1,10 @@
 use std::mem::transmute_copy;
 use libc::c_void;
 
-#[allow(non_camel_case_types)] pub struct anytype_t([u8; 8]);
+#[allow(non_camel_case_types)]
 #[repr(C)]
+pub struct anytype_t([u8; 8]);
+
 impl anytype_t {
     pub unsafe fn as_ptr(&self) -> *const c_void { transmute_copy(&self.0) }
     pub unsafe fn as_i32(&self) -> *const c_void { transmute_copy(&self.0) }
