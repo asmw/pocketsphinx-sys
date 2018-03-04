72e93aaa4994f2410155bed89eb7fba3a77f3f91
asmw <asmw@asmw.org>
2018-03-04 14:17:06 +0100

    Restrict not using pkgconfig to MSVC

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ extern crate pkg_config;
 use std::env;
 
 fn main() {
-    if !env::var("TARGET").unwrap().contains("windows") {
+    if !env::var("TARGET").unwrap().contains("msvc") {
         pkg_config::find_library("pocketsphinx").unwrap();
     }
 }
